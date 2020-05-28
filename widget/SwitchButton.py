9141ff04565758c80cc42f5c67c7287631eeec3c
S.Hag <s.hagmann@bgbaden-frauen.ac.at>
2020-05-28 18:10:42 +0200

    merge

--- widget/SwitchButton.py
+++ widget/SwitchButton.py
@@ -4,25 +4,26 @@ from PyQt5 import QtGui, QtWidgets, QtCore
 from PyQt5.Qt import Qt
 from PyQt5.QtGui import QPen, QBrush
 
+
 class SwitchButton(QtWidgets.QWidget):
-    
+
     def __init__(self, parent=None, w1="Ja", l1=12, w2="Nein", l2=33, width=60):
         super(SwitchButton, self).__init__(parent)
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
         self.__labeloff = QtWidgets.QLabel(self)
         self.__labeloff.setText(w2)
-        self.__labeloff.setStyleSheet("""color: rgb(120, 120, 120); margin-top:2px; font-weight: bold;""")
-        self.__background  = Background(self)
+        self.__labeloff.setStyleSheet("""color: rgb(120, 120, 120); font-weight: bold;""")
+        self.__background = Background(self)
         self.__labelon = QtWidgets.QLabel(self)
         self.__labelon.setText(w1)
-        self.__labelon.setStyleSheet("""color: rgb(255, 255, 255);  margin-top:2px; font-weight: bold;""")
-        self.__circle      = Circle(self)
-        self.__circlemove  = None
+        self.__labelon.setStyleSheet("""color: rgb(255, 255, 255); font-weight: bold;""")
+        self.__circle = Circle(self)
+        self.__circlemove = None
         self.__ellipsemove = None
-        self.__enabled     = True
-        self.__duration    = 100
-        self.__value       = False
+        self.__enabled = True
+        self.__duration = 100
+        self.__value = False
         self.setFixedSize(width, 24)
 
         self.__background.resize(20, 20)
@@ -30,28 +31,28 @@ class SwitchButton(QtWidgets.QWidget):
         self.__circle.move(2, 2)
         self.__labelon.move(l1, 2)
         self.__labeloff.move(l2, 2)
-        
+
         self.__parent = parent
-        
+
     def text(self):
         return "SwitchButton"
-    
+
     def getValue(self):
         ''' get the value '''
-        return self.__value    
-    
+        return self.__value
+
     def setValue(self, v):
         ''' set value '''
-        if v!=self.__value:
-            self.mousePressEvent(None) 
+        if v != self.__value:
+            self.mousePressEvent(None)
 
     def setDuration(self, time):
         self.__duration = time
 
     def mousePressEvent(self, event):
-        #fire event to parent
+        # fire event to parent
         self.__parent.toggleDev()
-        
+
         if not self.__enabled:
             return
 
@@ -62,16 +63,16 @@ class SwitchButton(QtWidgets.QWidget):
         self.__ellipsemove.setDuration(self.__duration)
 
         xs = 2
-        y  = 2
-        xf = self.width()-22
+        y = 2
+        xf = self.width() - 22
         hback = 20
         isize = QtCore.QSize(hback, hback)
-        bsize = QtCore.QSize(self.width()-4, hback)
+        bsize = QtCore.QSize(self.width() - 4, hback)
         if self.__value:
             xf = 2
-            xs = self.width()-22
+            xs = self.width() - 22
             bsize = QtCore.QSize(hback, hback)
-            isize = QtCore.QSize(self.width()-4, hback)
+            isize = QtCore.QSize(self.width() - 4, hback)
 
         self.__circlemove.setStartValue(QtCore.QPoint(xs, y))
         self.__circlemove.setEndValue(QtCore.QPoint(xf, y))
@@ -83,7 +84,6 @@ class SwitchButton(QtWidgets.QWidget):
         self.__ellipsemove.start()
         self.__value = not self.__value
 
-
     def paintEvent(self, event):
         s = self.size()
         qp = QtGui.QPainter()
@@ -92,16 +92,16 @@ class SwitchButton(QtWidgets.QWidget):
         pen = QtGui.QPen(QtCore.Qt.NoPen)
         qp.setPen(pen)
         qp.setBrush(QtGui.QColor(120, 120, 120))
-        #qp.drawRoundedRect(0, 0, s.width(), s.height(), 12, 12)
-        #lg = QtGui.QLinearGradient(35, 30, 35, 0)
-        #lg.setColorAt(0, QtGui.QColor(210, 210, 210, 255))
-        #lg.setColorAt(0.25, QtGui.QColor(255, 255, 255, 255))
-        #lg.setColorAt(0.82, QtGui.QColor(255, 255, 255, 255))
-        #lg.setColorAt(1, QtGui.QColor(210, 210, 210, 255))
-        
-        lg=QBrush(QtGui.QColor(180, 180, 180), Qt.SolidPattern)
+        # qp.drawRoundedRect(0, 0, s.width(), s.height(), 12, 12)
+        # lg = QtGui.QLinearGradient(35, 30, 35, 0)
+        # lg.setColorAt(0, QtGui.QColor(210, 210, 210, 255))
+        # lg.setColorAt(0.25, QtGui.QColor(255, 255, 255, 255))
+        # lg.setColorAt(0.82, QtGui.QColor(255, 255, 255, 255))
+        # lg.setColorAt(1, QtGui.QColor(210, 210, 210, 255))
+
+        lg = QBrush(QtGui.QColor(180, 180, 180), Qt.SolidPattern)
         qp.setBrush(lg)
-        qp.drawRoundedRect(1, 1, s.width()-2, s.height()-2, 10, 10)
+        qp.drawRoundedRect(1, 1, s.width() - 2, s.height() - 2, 10, 10)
 
         qp.setBrush(QtGui.QColor(210, 210, 210))
         qp.drawRoundedRect(2, 2, s.width() - 4, s.height() - 4, 10, 10)
@@ -124,6 +124,7 @@ class SwitchButton(QtWidgets.QWidget):
             qp.drawRoundedRect(3, 3, s.width() - 6, s.height() - 6, 7, 7)
         qp.end()
 
+
 class Circle(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super(Circle, self).__init__(parent)
@@ -131,7 +132,7 @@ class Circle(QtWidgets.QWidget):
         self.setFixedSize(20, 20)
 
     def paintEvent(self, event):
-        #s = self.size()
+        # s = self.size()
         qp = QtGui.QPainter()
         qp.begin(self)
         qp.setRenderHint(QtGui.QPainter.Antialiasing, True)
@@ -145,19 +146,19 @@ class Circle(QtWidgets.QWidget):
         rg.setColorAt(1, QtGui.QColor(205, 205, 205))
         qp.setBrush(QtGui.QBrush(rg))
         '''
-        qp.drawEllipse(1,1, 18, 18)
+        qp.drawEllipse(1, 1, 18, 18)
 
         qp.setBrush(QtGui.QColor(210, 210, 210))
         qp.drawEllipse(2, 2, 16, 16)
 
         if self.__enabled:
-            lg = QtGui.QLinearGradient(3, 18,20, 4)
+            lg = QtGui.QLinearGradient(3, 18, 20, 4)
             lg.setColorAt(0, QtGui.QColor(255, 255, 255, 255))
             lg.setColorAt(0.55, QtGui.QColor(230, 230, 230, 255))
             lg.setColorAt(0.72, QtGui.QColor(255, 255, 255, 255))
             lg.setColorAt(1, QtGui.QColor(255, 255, 255, 255))
             qp.setBrush(lg)
-            qp.drawEllipse(3,3, 14, 14)
+            qp.drawEllipse(3, 3, 14, 14)
         else:
             lg = QtGui.QLinearGradient(3, 18, 20, 4)
             lg.setColorAt(0, QtGui.QColor(230, 230, 230))
@@ -167,6 +168,7 @@ class Circle(QtWidgets.QWidget):
             qp.setBrush(lg)
             qp.drawEllipse(3, 3, 14, 14)
         qp.end()
+
 
 class Background(QtWidgets.QWidget):
     def __init__(self, parent=None):
@@ -181,7 +183,7 @@ class Background(QtWidgets.QWidget):
         qp.setRenderHint(QtGui.QPainter.Antialiasing, True)
         pen = QtGui.QPen(QtCore.Qt.NoPen)
         qp.setPen(pen)
-        qp.setBrush(QtGui.QColor(154,205,50))
+        qp.setBrush(QtGui.QColor(154, 205, 50))
         if self.__enabled:
             qp.setBrush(QtGui.QColor(154, 190, 50))
             qp.drawRoundedRect(0, 0, s.width(), s.height(), 10, 10)
